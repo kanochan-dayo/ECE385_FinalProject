4f0f2697f03c10956f0eb16563e66e9410436e80
kanochan-dayo <inwin_leo@qq.com>
2022-11-29 15:58:52 -0600

    huhu

--- music_demo/software/initializer/sgtl5000_test.c
+++ music_demo/software/initializer/sgtl5000_test.c
@@ -16,83 +16,6 @@
 #include "sgtl5000/GenericTypeDefs.h"
 #include "sgtl5000/sgtl5000.h"
 
-
-void setLED(int LED)
-{
-	IOWR_ALTERA_AVALON_PIO_DATA(LEDS_PIO_BASE, (IORD_ALTERA_AVALON_PIO_DATA(LEDS_PIO_BASE) | (0x001 << LED)));
-}
-
-void clearLED(int LED)
-{
-	IOWR_ALTERA_AVALON_PIO_DATA(LEDS_PIO_BASE, (IORD_ALTERA_AVALON_PIO_DATA(LEDS_PIO_BASE) & ~(0x001 << LED)));
-
-}
-
-void printSignedHex0(signed char value)
-{
-	BYTE tens = 0;
-	BYTE ones = 0;
-	WORD pio_val = IORD_ALTERA_AVALON_PIO_DATA(HEX_DIGITS_PIO_BASE);
-	if (value < 0)
-	{
-		setLED(11);
-		value = -value;
-	}
-	else
-	{
-		clearLED(11);
-	}
-	//handled hundreds
-	if (value / 100)
-		setLED(13);
-	else
-		clearLED(13);
-
-	value = value % 100;
-	tens = value / 10;
-	ones = value % 10;
-
-	pio_val &= 0x00FF;
-	pio_val |= (tens << 12);
-	pio_val |= (ones << 8);
-
-	IOWR_ALTERA_AVALON_PIO_DATA(HEX_DIGITS_PIO_BASE, pio_val);
-}
-
-void printSignedHex1(signed char value)
-{
-	BYTE tens = 0;
-	BYTE ones = 0;
-	DWORD pio_val = IORD_ALTERA_AVALON_PIO_DATA(HEX_DIGITS_PIO_BASE);
-	if (value < 0)
-	{
-		setLED(10);
-		value = -value;
-	}
-	else
-	{
-		clearLED(10);
-	}
-	//handled hundreds
-	if (value / 100)
-		setLED(12);
-	else
-		clearLED(12);
-
-	value = value % 100;
-	tens = value / 10;
-	ones = value % 10;
-	tens = value / 10;
-	ones = value % 10;
-
-	pio_val &= 0xFF00;
-	pio_val |= (tens << 4);
-	pio_val |= (ones << 0);
-
-	IOWR_ALTERA_AVALON_PIO_DATA(HEX_DIGITS_PIO_BASE, pio_val);
-}
-
-
 int setup_i2c()
 {
 	ALT_AVALON_I2C_DEV_t *i2c_dev; //pointer to instance structure
@@ -175,7 +98,7 @@ int setup_i2c()
 	printf( "CHIP_ADCDAC_CTRL register: %x\n", SGTL5000_Reg_Rd (i2c_dev, SGTL5000_CHIP_ADCDAC_CTRL));
 	printf( "CHIP_PAD_STRENGTH register: %x\n", SGTL5000_Reg_Rd (i2c_dev, SGTL5000_CHIP_PAD_STRENGTH));
 
-	SGTL5000_Reg_Wr(i2c_dev, SGTL5000_CHIP_ANA_HP_CTRL, ((0x0028<<8)|0x0028));
+	SGTL5000_Reg_Wr(i2c_dev, SGTL5000_CHIP_ANA_HP_CTRL, ((0x0030<<8)|0x0030));
 
 
 
