48ee34afbe620df6b496f78460327944d611d233
kanochan-dayo <inwin_leo@qq.com>
2022-12-01 20:56:33 -0600

    generate new_data

--- sprite_bytes/to_background_mif.py
+++ sprite_bytes/to_background_mif.py
@@ -1,30 +1,29 @@
 
-input_file = "background.txt"
-output_file = "background.dat"
+import os
 
-# Read the input file
-f=open(input_file, "r")
-f2=open(output_file, "wb")
-# convert the file to binary
-# convert string of hex value to bytes
-# line="0"
-line1="0"
-# print(line1.upper())
-count=0
-for line in f:
-    count=count+1
-    line=line.strip()
-    if len(line) ==1:
-        line1="0"+line.upper()
-    else:
-        line1=line.upper()
-    try:
+txt_files = [f for f in os.listdir('.') if f.endswith('.txt')]
+for input_file in txt_files:
+    output_file = input_file.replace('.txt', '.dat')
+
+    # Read the input file
+    f=open(input_file, "r")
+    f2=open(output_file, "wb")
+    # convert the file to binary
+    # convert string of hex value to bytes
+    # line="0"
+    line1="0"
+    # print(line1.upper())
+    count=0
+    for line in f:
+        count=count+1
+        line=line.strip()
+        if len(line) ==1:
+            line1="0"+line.upper()
+        else:
+            line1=line.upper()
         f2.write(bytes.fromhex(line1))
-    except:
-        print("Error")
-        print(len(line))
-        print(line1)
-        print(count)
-        break
+    f.close()
+    f2.close()
+    
 
 
