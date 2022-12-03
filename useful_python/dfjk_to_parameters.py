2d287aee3788c1a6cf9f315fef068f4867972a24
kanochan-dayo <inwin_leo@qq.com>
2022-12-03 14:04:34 -0600

    Update dfjk_to_parameters.py

--- useful_python/dfjk_to_parameters.py
+++ useful_python/dfjk_to_parameters.py
@@ -11,9 +11,11 @@ d_pre="00"
 f_pre="00"
 j_pre="00"
 k_pre="00"
-
+pretime=0
+line_num=0
 with open(imput_file) as f:
     for line in f:
+        line_num+=1
         if not line:
             continue
         line = line.strip()
@@ -21,61 +23,78 @@ with open(imput_file) as f:
         Key_type = line[0]
         if Key_type == "D":
             if (d_pre == "01" and line[1:3] != "10") or (d_pre != "01" and line[1:3] == "10"):
-                print("Error: D key is error in line: "+line+"at index: "+str(f.tell()))
+                print("Error: D key is error(长短键种类错误) in line: "+line+"at index: "+str(line_num))
                 sys.exit()
-            d_pre = line[1:3]
             number = line[3:]
             number=float(number)
+            if(number<pretime):
+                print("Error: D key is error(时间顺序错误) in line: "+line+"at index: "+str(line_num))
+                sys.exit()
+            pretime=number
             number=round(number*1250/21)
             number=str(bin(number))[2:]
             number=number.zfill(14)
+            d_pre = line[1:3]
             out=open(output_files[0], "a")
-            out.write("16'b"+line[1:3]+number+",\n")
+            out.write("16'b"+line[1:3]+number+",        \\\\ Line"+line+"at index "+str(line_num)+"\n")
             out.close()
             continue
         if Key_type == "F":
             if (f_pre == "01" and line[1:3] != "10") or (f_pre != "01" and line[1:3] == "10"):
-                print("Error: F key is error in line: "+line+"at index: "+str(f.tell()))
+                print("Error: F key is error(长短键种类错误) in line: "+line+"at index: "+str(line_num))
                 sys.exit()
-            f_pre = line[1:3]
             number = line[3:]
             number=float(number)
+            if(number<pretime):
+                print("Error: F key is error(时间顺序错误) in line: "+line+"at index: "+str(line_num))
+                sys.exit()
+            pretime=number
             number=round(number*1250/21)
             number=str(bin(number))[2:]
             number=number.zfill(14)
+            f_pre = line[1:3]
             out=open(output_files[1], "a")
-            out.write("16'b"+line[1:3]+number+",\n")
+            out.write("16'b"+line[1:3]+number+",        \\\\ Line"+line+"at index "+str(line_num)+"\n")
             out.close()
             continue
         if Key_type == "J":
             if (j_pre == "01" and line[1:3] != "10") or (j_pre != "01" and line[1:3] == "10"):
-                print("Error: J key is error in line: "+line+"at index: "+str(f.tell()))
+                print("Error: J key is error(长短键种类错误) in line: "+line+"at index: "+str(line_num))
                 sys.exit()
-            j_pre = line[1:3]
             number = line[3:]
             number=float(number)
+            if(number<pretime):
+                print("Error: J key is error(时间顺序错误) in line: "+line+"at index: "+str(line_num))
+                sys.exit()
+            pretime=number
             number=round(number*1250/21)
             number=str(bin(number))[2:]
             number=number.zfill(14)
+            j_pre = line[1:3]
             out=open(output_files[2], "a")
-            out.write("16'b"+line[1:3]+number+",\n")
+            out.write("16'b"+line[1:3]+number+",        \\\\ Line"+line+"at index "+str(line_num)+"\n")
             out.close()
             continue
         if Key_type == "K":
             if (k_pre == "01" and line[1:3] != "10") or (k_pre != "01" and line[1:3] == "10"):
-                print("Error: K key is error in line: "+line+"at index: "+str(f.tell()))
+                print("Error: K key is error(长短键种类错误) in line: "+line+"at index: "+str(line_num))
                 sys.exit()
-            k_pre = line[1:3]
             number = line[3:]
             number=float(number)
+            if(number<pretime):
+                print("Error: K key is error(时间顺序错误) in line: "+line+"at index: "+str(line_num))
+                sys.exit()
+            pretime=number
             number=round(number*1250/21)
             number=str(bin(number))[2:]
             number=number.zfill(14)
+            k_pre = line[1:3]
             out=open(output_files[3], "a")
-            out.write("16'b"+line[1:3]+number+",\n")
+            out.write("16'b"+line[1:3]+number+",        \\\\ Line"+line+"at index "+str(line_num)+"\n")
             out.close()
             continue
-        print("Error: Key type is error in line: "+line+"at index: "+str(f.tell()))
+        
+        print("Error: Key type is error(按键类型不属于dfjk) in line: "+line+"at index: "+str(line_num))
         sys.exit()
         
         
